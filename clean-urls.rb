847023fd5f26d32a2bf8cf609d3331345c75c82e
Will Norris <will@willnorris.com>
2014-08-13 17:20:05 -0700

    add support for clean urls for collection documents

--- clean-urls.rb
+++ clean-urls.rb
@@ -90,7 +90,21 @@ module Jekyll
 
     alias_method :url_without_clean_urls, :url
     alias_method :url, :url_with_clean_urls
+  end
 
+  class Document
+    # Obtain destination path, appending file extension for documents that lack one.
+    def destination_with_clean_urls(dest)
+      path = destination_without_clean_urls(dest)
+      ext = Jekyll::Renderer.new(site, self).output_ext
+      if !(asset_file? || yaml_file?) && path !~ /#{ext}$/
+        path += ext
+      end
+      path
+    end
+
+    alias_method :destination_without_clean_urls, :destination
+    alias_method :destination, :destination_with_clean_urls
   end
 
   module Paginate
